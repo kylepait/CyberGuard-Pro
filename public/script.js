1c4e4faaac903d3d0cbb0ba4786e2d53fb06c822
kylepait <kylepait@gmail.com>
2024-01-25 13:56:20 -0500

    edited the auth const

--- public/script.js
+++ public/script.js
@@ -1,10 +1,8 @@
-// script.js
+
 
 // Import the functions you need from the SDKs you need
 /** 
-const { initializeApp } = require("firebase/app");
-const { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } = require('firebase/auth');
-const { getAnalytics } = require("firebase/analytics");
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 */
