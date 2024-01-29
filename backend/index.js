8f3485cf54367b469ebe5e26286b1b19f7252785
kylepait <kylepait@gmail.com>
2024-01-29 10:14:29 -0500

    connecting backend to database

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
+const pool = require('./config/db.js')
 
+require(dotenv/config);
+
+//Database connection
+pool.getConnection( (err, conn) =>{
+    if (err) throw err;
+})
+
+/** 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
-
-const PORT = 4000; // backend routing port
+*/
+const PORT = 3306; // backend routing port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
