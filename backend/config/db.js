83eb93b9ca2e993feaa9553d477e6a2cfcfd0e15
kylepait <kylepait@gmail.com>
2024-01-29 10:51:39 -0500

    trying to fix port issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT
 
 });
 
--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ const pool = require('./config/db.js')
 
 require('dotenv/config');
 
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('/', routesHandler);
+
 //Database connection
 pool.getConnection( (err, conn) =>{
     if (err) throw err;
@@ -28,7 +33,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
 */
-const PORT = process.env.PORT || 3306; // backend routing port
+const PORT = process.env.PORT || 4000; // backend routing port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
