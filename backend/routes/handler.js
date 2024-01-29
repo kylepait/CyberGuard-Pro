63c7434316868ae2be558b9651d011e8c3d085ee
kylepait <kylepait@gmail.com>
2024-01-29 11:50:39 -0500

    now the backend port 4000 will show list of userstable from db

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../config/db.js');
 
-router.get('/Signup', (req, res) => {
-    const str = [
-        {
-            "name": "Kyle Pait",
-            "password": "this is not my password",
-            "email": "kylepait@gmail.com"
+router.get('/Signup', async (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {
+            console.error('Error getting connection from pool:', err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
         }
-    ];
-    res.end(JSON.stringify(str));
+
+        const qry = 'SELECT u.username, u.password, u.organization_id FROM users_table as u';
+        
+        connection.query(qry, (err, result) => {
+            connection.release();
+
+            if (err) {
+                console.error('Error executing query:', err);
+                res.status(500).json({ error: 'Internal Server Error' });
+                return;
+            }
+
+            res.json(result);
+        });
+    });
 });
 
 router.post('/addUser', (req, res) => {
