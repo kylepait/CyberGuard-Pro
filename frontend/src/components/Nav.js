853d316e0b3b84e0c977c8bb94111cad1e5be192
kylepait <kylepait@gmail.com>
2024-02-28 07:49:38 -0500

    more visual changes

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Nav(){
-
-    return(
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark top">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark top">
+            <Link to="/" className="navbar-brand">Welcome to CyberGuard Pro</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div id="navMainMenu" class="navbar-collapse collapse">
-                <div class="navbar-nav ml-auto">
+            <div id="navMainMenu" className="navbar-collapse collapse">
+                <div className="navbar-nav ml-auto">
                     <Link to='/' className="nav-item nav-link active">Login</Link>
                     <Link to='/signup' className="nav-item nav-link">Signup</Link>
                     <Link to='/trainingModule' className="nav-item nav-link">Training Module</Link>
@@ -18,7 +18,6 @@ function Nav(){
             </div>
         </nav>
     );
-
 }
 
 export default Nav;
